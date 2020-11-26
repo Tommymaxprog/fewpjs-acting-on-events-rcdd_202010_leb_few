4042ec981732d73201a29446db4cddd32f1eb333
sagacious-shell-7968 <tommysytc.tc@gmail.com>
2020-11-26 09:42:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ document.addEventListener("keydown", function(e) {
 function moveDodgerRight() {
   let rightNumbers = dodger.style.right.replace("px", "");
   let right = parseInt(rightNumbers, 10);
- 
+ let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
   if (right > 0) {
     dodger.style.left = `${left + 1}px`;
   }
