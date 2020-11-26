e1ef9fc8ac82b4b586893a7e8b9261c59bcf3dbb
sagacious-shell-7968 <tommysytc.tc@gmail.com>
2020-11-26 09:51:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let dodger = document.getElementById("dodger");
 function moveDodgerLeft() {
   let leftNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(leftNumbers, 10);
-  console.log(dodger.style.right);
+
   
  
   if (left > 0) {
@@ -20,9 +20,9 @@ function moveDodgerRight() {
   let right = parseInt(rightNumbers, 10);
  let leftNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(leftNumbers, 10);
-  if (right > 40) {
+  
     dodger.style.left = `${left + 1}px`;
-  }
+  
 }
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowRight") {
